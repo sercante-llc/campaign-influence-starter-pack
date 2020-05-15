7765f2571463cbad354b660843ce11b098ae833e
adamerstelle <adam@erstelle.ca>
2020-05-15 09:49:17 -0400

    get the scratch org working just right for Lauren

--- setupScratchOrg.sh
+++ setupScratchOrg.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Installing Pardot package: 'Pardot Package@4.68.0.1'"
+echo "Installing Pardot package (pi): 'Pardot Package@4.68.0.1'"
 sfdx force:package:install --package 04t1W000000kpBDQAY -w 20
-#echo "Installing Sercante Core Objects package: 'Pardot Internal Integration@1.6.0.1'"
+#echo "Installing Pardot Integration package (b2bma): 'Pardot Internal Integration@1.6.0.1'"
 #sfdx force:package:install --package 04tf4000001VsyYAAS -w 20
 
-# echo "Doing initial push of source"
-# sfdx force:source:push
+echo "Doing initial push of source"
+sfdx force:source:push
+
+echo "Assigning permission set"
+sfdx force:user:permset:assign --permsetname "Campaign_Influence_Sercante_Labs"
 
 # echo "Setting up test data"
 # cd sfdx-data
